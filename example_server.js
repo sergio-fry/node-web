b41e437c1adad6289257234d27afa75bca4e5a0c
Ilya Muromets <Azureguardian@yandex.ru>
2017-07-19 00:00:20 +0300

    Example node WEB-server

---
+++ example_server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+process.on( 'SIGINT', function() {
+    console.log( "\nShut down from SIGINT (Ctrl-C)" );
+    process.exit( );
+});
+
